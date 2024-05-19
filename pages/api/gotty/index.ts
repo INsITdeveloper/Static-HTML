ee93612dace6eef97c696f7dd5552a9b90c4d5ec
ユスプカクウ <156079487+HindiaFtNpc@users.noreply.github.com>
2024-05-19 18:45:50 +0700

    Update and rename index.js to index.ts

--- pages/api/gotty/index.ts
+++ pages/api/gotty/index.ts
@@ -1,6 +1,7 @@
+import { NextApiRequest, NextApiResponse } from 'next';
 const useGotty = require('use-gotty');
 
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     const httpServer = await useGotty(process.cwd(), ['sh'], 6666);
     res.status(200).json({ url: httpServer.url });

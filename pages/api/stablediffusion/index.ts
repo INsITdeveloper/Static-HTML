68c24de2cd3c987a967ab9fe3bd2df33b56cfd47
ユスプカクウ <156079487+HindiaFtNpc@users.noreply.github.com>
2024-05-21 01:14:20 +0700

    Create index.ts

---
+++ pages/api/stablediffusion/index.ts
@@ -0,0 +1,24 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+const { stablediffusion } = require("gpti");
+
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+    const prompt = req.query.q as string || " ";
+
+    stablediffusion.v2({
+    prompt: prompt,
+    data: {
+        prompt_negative: "",
+        guidance_scale: 9
+    }
+        ],
+    }, (err: Error | null, data: any) => { // Menggunakan tipe `any` untuk data
+        if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+            res.status(200).json(data);
+        }
+    });
+};
+
+export default handler;

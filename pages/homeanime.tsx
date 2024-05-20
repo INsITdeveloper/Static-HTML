fde894b7e614031d970524f65749821f868c5556
ユスプカクウ <156079487+HindiaFtNpc@users.noreply.github.com>
2024-05-20 14:54:40 +0700

    Create homeanime.tsx

---
+++ pages/homeanime.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// Buat komponen React untuk halaman Anime
+const AnimePage: React.FC = () => {
+  return (
+    <div>
+      <h1>Halaman Anime</h1>
+      <p>Ini adalah halaman anime dari https://sh.zanixon.xyz/anime.</p>
+      {/* Isi halaman dengan konten sesuai kebutuhan */}
+    </div>
+  );
+};
+
+// Ekspor komponen sebagai default
+export default AnimePage;
